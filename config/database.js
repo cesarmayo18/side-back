1cabee84a01a9618d713df97db59be90cb4b02c2
Cesar Mayo <cesarmayo@Cesars-MacBook-Pro.local>
2023-09-03 20:53:41 -0300

    fix(google-auth) add procy true configuration

--- config/database.js
+++ config/database.js
@@ -55,8 +55,16 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            false
+          ),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
