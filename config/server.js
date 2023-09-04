d9fee80effe99e3110fafa77b3eab4634973feb2
Cesar Mayo <cesarmayo@Cesars-MacBook-Pro.local>
2023-09-03 21:46:59 -0300

    chore(server): config server url

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   proxy: true,
+  url:'https://side-project-c232b6bb9e71.herokuapp.com/',
   app: {
     keys: env.array('APP_KEYS'),
   },
