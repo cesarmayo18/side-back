fd83d0dfd867de21dd6a228e40f1ad778719c4b1
Cesar Mayo <cesarmayo@Cesars-MacBook-Pro.local>
2023-09-03 21:11:16 -0300

    chore(database): config ssl connection to database

--- config/database.js
+++ config/database.js
@@ -55,16 +55,8 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            false
-          ),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
