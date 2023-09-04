54ef78eecab5c6826159db374f63a8ccf787f03c
Cesar Mayo <cesarmayo@Cesars-MacBook-Pro.local>
2023-09-03 21:56:06 -0300

    fix(server): remove host from server config

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  // host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   proxy: true,
-  url: env('URL', '0.0.0.0'),
+  url: env('URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
